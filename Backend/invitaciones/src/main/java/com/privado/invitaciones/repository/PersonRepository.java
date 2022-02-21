f291e899eb5f7c28c00996a045b02bc6cb13528d
Freddy Brito <freddybrito1@gmail.com>
2022-02-21 00:30:14 -0300

    Definida la Capa de comunicacion con la BD para Persona

---
+++ Backend/invitaciones/src/main/java/com/privado/invitaciones/repository/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.privado.invitaciones.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.privado.invitaciones.models.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+}
